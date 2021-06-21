6ddce577456bebf94819653099083c63b9230b2e
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-21 17:05:34 -0500

    java update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("Hello, world!");
-    }
 
+        int myFavoriteNumber = 20;
+        var String = "myString";
+
+    }
 }
