790d7af3bad5c5055e9551e23e3e884fb7c3af12
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-21 22:41:27 -0500

    java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,26 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, world!");
 
-        int myFavoriteNumber = 20;
-        var String = "myString";
+        // .1
+//        int myFavoriteNumber = 20;
+//        System.out.println(myFavoriteNumber);
+
+        // .2
+//        var myString = "Music";
+//        System.out.println(myString);
+
+        // .3
+//        char myString = 'm';
+//        System.out.println(myString);
+
+        // .4
+//        var myString = 3.14159;
+//        System.out.println(myString);
+
+        // .5
+//        long myNumber;
+//        System.out.println(myNumber);
+        // variable myNumber might not have been initialize
 
     }
 }
