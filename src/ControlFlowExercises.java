dafa286fca39e042fda1eb8c8e55db81a6890b1d
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-22 22:10:34 -0500

    update

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -15,14 +15,20 @@ public class ControlFlowExercises {
 //            System.out.println("The number is now: " + i * 2);
 //            i++;
 //        }while(i <= 50);
+        // Not the right answer. ask about it tomorrow
 
         // c. For Loop
 //        for(int i = 5; i <= 15; i++){
 //            System.out.println("i is: " + i);
 //        }
 
-        for(int i = 0; i <= 100; i++){
-            System.out.println("The number is now: " + i * 2);
-        }
+//        for(int i = 0; i <= 100; i++){
+//            System.out.println("The number is now: " + i * 2);
+        // Not the right answer. Ask about it tomorrow
+
+
+            // .2 Fizzbuzz
+
+//        }
     }
 }
