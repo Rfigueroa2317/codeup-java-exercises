45f58373371fb41703e103471a5e44e567f3cd15
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-28 13:56:21 -0500

    update

--- src/Bob.java
+++ src/Bob.java
@@ -5,5 +5,16 @@ public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String answer1 = "Sure.";
+        String answer2 = "Whoa chill out!";
+        String answer3 = "Whoa, chill out!";
+        String answer4 = "Whatever.";
+        System.out.println("");
+        String input = sc.next();
+
+        if(input.equals("")){
+            System.out.println();
+        }
+
     }
 }
