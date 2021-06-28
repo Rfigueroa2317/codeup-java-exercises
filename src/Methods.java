0c3ed1252351efad97e0d333418a7eb8e85f2d7a
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-28 15:30:52 -0500

    update

---
+++ src/Methods.java
@@ -0,0 +1,59 @@
+public class Methods {
+
+
+//    Different types of methods
+
+//    main
+
+//    public static void main(String[] args) {
+//        System.out.println("hello world");
+//
+//        System.out.println(greeting("twenty-four")); //24
+//
+//        return; // acts like a break;
+//
+//    }
+//
+//    public static int greeting(String age){
+//        System.out.println("age: " + age);
+//        int indexOfR = age.indexOf('r');
+//        return indexOfR;
+//    }
+
+//    syntax
+//    public - whether - how visible your method is to other classes.
+//    static - available for this class and not an instance of a class
+//    void   - define what the data type is of what's being returned
+//              (if anything is being returned)
+//    main, nameOfTheMethod - name of the method - you come up with that
+//    (dataType parameters) - parameters go in the parenthesis within
+//              the data type defined
+//    {/* your code /code block goes here * (/ }
+
+//    1) it for sure is going to have a return statement
+//    2) it's going to expect the return to actually return a String
+//    data type
+//    public static String greeting(){
+//        return "hello world";
+//    }
+
+
+//    TODO: create a new method that accepts the name of a basketball
+//     team (ex: the spurs) and returns a String that says
+//     "[basketballTeam] is the best team.". Invoke that method within
+//     your main method (remember to pass in the name of the basketball
+//     team). print out the result of the new method.
+
+    public static void main(String[] args) {
+        return;
+    }
+
+    public static String BasketballTeam(String[] args){
+//
+        System.out.println("");
+    }
+
+//     TODO: create a new method that accepts the string created from the
+//      first method and returns the number of characters in that string.
+
+}
