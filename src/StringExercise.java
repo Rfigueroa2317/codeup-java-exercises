05ac52ea441f8bcae873a8aa7a5ed358a26b204a
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-28 11:44:13 -0500

    update

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+
+
+
+
+    }
+}
