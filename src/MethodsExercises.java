ef966bc9e0ea0ee7c35b1517985b67a32ace981a
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-29 16:11:42 -0500

    update

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,20 +2,24 @@ import java.util.Scanner;
 
 public class MethodsExercises {
 
-    // 1. Basic Arithmetic. Create four separate methods.
+    // 1. Basic Arithmetic.
+
+    // .a Create four separate methods.
     // Each will perform an arithmetic operation:
 
-    // .a Addition
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println(" Basic Arithmetic:  ");
-        int addNumber = scanner.nextInt();
-//        Addition(addNumber);
-//        Subtraction(addNumber);
-//        Multiplication(addNumber);
-        Division(addNumber);
-    }
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(" Basic Arithmetic:  ");
+//        int addNumber = scanner.nextInt();
+////        Addition(addNumber);
+////        Subtraction(addNumber);
+////        Multiplication(addNumber);
+////        Division(addNumber);
+////        Modulus(addNumber);
+//    }
+
+    // Addition
 
 //    public static int Addition(int s){
 //        Scanner scanner = new Scanner(System.in);
@@ -29,7 +33,7 @@ public class MethodsExercises {
 //        return num3;
 //    }
 
-    // b. Subtraction
+    // Subtraction
 
 //    public static int Subtraction(int s){
 //        Scanner scanner = new Scanner(System.in);
@@ -43,6 +47,8 @@ public class MethodsExercises {
 //        return num3;
 //    }
 
+    // Multiplication
+
 //    public static int Multiplication(int s){
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println(s);
@@ -55,17 +61,65 @@ public class MethodsExercises {
 //        return num3;
 //    }
 
-    public static int Division(int s){
+    // Division
+
+//    public static int Division(int s){
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(s);
+//        int num1 = scanner.nextInt();
+//        System.out.println("Enter first number: " + num1);
+//        int num2 = scanner.nextInt();
+//        System.out.println("Enter second number: " + num2);
+//        int num3 = num1 / num2;
+//        System.out.println("The division of both numbers is: " + num3);
+//        return num3;
+//    }
+
+    // d. Modulus
+
+//    public static int Modulus(int s) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(s);
+//        int num1 = scanner.nextInt();
+//        System.out.println("Enter first number: " + num1);
+//        int num2 = scanner.nextInt();
+//        System.out.println("Enter second number: " + num2);
+//        int num3 = num1 % num2;
+//        System.out.println("The remainder of both numbers is: " + num3);
+//        return num3;
+//
+//    }
+
+    // .2 Create a method that validates that user input is in a certain range
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(s);
-        int num1 = scanner.nextInt();
-        System.out.println("Enter first number: " + num1);
-        int num2 = scanner.nextInt();
-        System.out.println("Enter second number: " + num2);
-        int num3 = num1 / num2;
-        System.out.println("The division of both numbers is: " + num3);
-        return num3;
+
     }
+
+        public static int getInteger(int min, int max){
+        int userInput = getInteger(1, 10);
+        return getInteger();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 // function addTwoNumbers(param1, param2){
