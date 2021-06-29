2c9d7ca6f756e7a8a8c58c8afeddd4c2aa40dea5
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-29 10:02:40 -0500

    update

--- src/WarmupJune29.java
+++ src/WarmupJune29.java
@@ -9,18 +9,47 @@ public class WarmupJune29 {
 //     expect you to return: 18. but if I tell you my name first: Laura --
 //     false -- 18. I expect you to return "Laura" ... etc.
 
+
+    // *** My answer *** \\
+    // make sure you work on it and try to solve it \\
+
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        return sayHello();
+//
+//    }
+//
+//
+//    public static void sayHello(String[] s) {
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.nextLine();
+//
+//        String userInfo = "Hello there ";
+//        System.out.println("hello there " + userInfo);
+//    }
+
+    // *** PRACHI'S ANSWER *** \\
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        return sayHello();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name, age, Do you have pets?");
+        String fact1 = scanner.next();
+        String fact2 = scanner.next();
+        String fact3 = scanner.next();
+
+        System.out.println(getTheFact(fact1));
 
     }
 
-
-    public static void sayHello(String[] s) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-
-        String userInfo = "Hello there ";
-        System.out.println("hello there " + userInfo);
+    private static String getTheFact(String fact1) {
+        return fact1;
     }
+
+    private static int getTheFact(int fact1) {
+        System.out.println("in method 2");
+        return + fact1;
+    }
+
 }
