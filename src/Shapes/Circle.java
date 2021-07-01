ddfcf317b5b0cbcf181c92dfe6567e63b414337e
ricardofigueroa <rfigueroa2317@gmail.com>
2021-06-30 22:23:14 -0500

    update

--- src/Shapes/Circle.java
+++ src/Shapes/Circle.java
@@ -1,4 +1,18 @@
 package Shapes;
 
 public class Circle {
+
+    private int radius;
+
+    public Circle(double radius){
+
+    }
+
+    public double getArea(){
+
+    }
+
+    public double getCircumference(){
+
+    }
 }
