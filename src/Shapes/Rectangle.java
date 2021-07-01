cde7e03c45c81ae03f470f094ae0754a36e90533
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-01 16:42:19 -0500

    update

--- src/Shapes/Rectangle.java
+++ src/Shapes/Rectangle.java
@@ -1,6 +1,32 @@
 package Shapes;
 
+import oop.Person;
+
 public class Rectangle {
 
+    protected int Area;
+    protected int Perimeter;
 
+    public Rectangle(int length,int width){
+
+       this.Area = Area;
+       this.Perimeter = Perimeter;
+
+    }
+
+    public int getArea() {
+        return Area;
+    }
+
+    public void setArea(int area) {
+        Area = area;
+    }
+
+    public int getPerimeter() {
+        return Perimeter;
+    }
+
+    public void setPerimeter(int perimeter) {
+        Perimeter = perimeter;
+    }
 }
