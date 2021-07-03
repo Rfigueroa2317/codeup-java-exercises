8988b714a7db35077750967858f7be263efe1a40
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-02 21:42:18 -0500

    update

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -6,5 +6,17 @@ public class StringExercise {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        // .1
+
+        String message = "We don't need no education. ";
+        message = message + "We don't need no thought control";
+        System.out.println(message);
+
+        System.out.println("Check 'this' out!, " + message + " inside of " + message + "s!");
+
+        message = "In windows, the main drive is usually C:\\";
+        System.out.println(message);
+
+
     }
 }
