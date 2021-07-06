558845ea18818e0cb04a0c9a82b3a3097484a872
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-06 14:04:38 -0500

    update

---
+++ src/Fruit.java
@@ -0,0 +1,22 @@
+abstract class Fruit {
+
+    public String mango;
+    public String color;
+    public String type;
+    public String size;
+
+    public Fruit(String nameOfFruit, String color, String size){
+        this.mango = mango;
+        this.color = color;
+        this.type = type;
+        this.size = size;
+    }
+
+    
+
+    public String isFruit(){
+        return "I am Fruit";
+    }
+
+
+}
--- src/Person.java
+++ src/Person.java
@@ -3,27 +3,29 @@ import java.util.Scanner;
 // .1
 
  public class Person {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    private String name;
+//
+//    public String getName(){
+//        return name;
+//        // TODO: return the person's name
+//    }
+//
+//    public void setName(String Ricardo){
+//        // TODO: change the name property to the passed value
+//        this.name = "Ricardo";
+//
+//    }
+//
+//    public void sayHello(){
+//        // TODO: print a message to the console using
+//        //  the person's name
+//        System.out.println("Hello!, my name is %s!" + name);
 
-    }
+//    }
 
-    private String name;
 
-    public String getName(){
-        return name;
-        // TODO: return the person's name
-    }
-
-    public void setName(String Ricardo){
-        // TODO: change the name property to the passed value
-        this.name = "Ricardo";
-
-    }
-
-    public void sayHello(){
-        // TODO: print a message to the console using
-        //  the person's name
-        System.out.println("Hello!, my name is %s!" + name);
-
-    }
 }
