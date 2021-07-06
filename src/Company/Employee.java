e908c2e32aa37432435803f657eac45656c92509
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-06 10:16:02 -0500

    update

---
+++ src/Company/Employee.java
@@ -0,0 +1,24 @@
+package Company;
+
+// 1. create an abstract class using the 'abstract' keyword.
+// abstract classes cannot be instantiated
+// instantiation - to create an instance (an object) of that class
+// create a subclass from the abstract and then instantiate the subclass
+
+abstract class Employee {
+    // define properties
+    private String salary;
+    private String name;
+
+    // if you don't define method in abstract class, you must mark it
+    // abstract
+//    public abstract String doWork();
+
+    public String doWork(){
+        return "I am working";
+    }
+
+}
+
+
+
---
+++ src/Company/Manager.java
@@ -0,0 +1,7 @@
+package Company;
+
+// extends keyword to create subclass of the Employee abstract class
+public class Manager extends Employee {
+    // every child class must define the method
+
+}
