10ff581ab7ed4c305d77cc9d38de94bc5d33d8ad
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-07 13:36:42 -0500

    update

---
+++ src/ArraysExercises.java
@@ -0,0 +1,10 @@
+public class ArraysExercises {
+
+    public static void main(String[] args) {
+        System.out.println("Arrays Exercise!");
+
+        int[] numbers = {1,2,3,4,5};
+        System.out.println(numbers);
+    }
+
+}
