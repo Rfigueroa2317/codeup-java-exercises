6555fc885feaf4ba357559e1361e8f08e8e9be27
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-08 09:52:22 -0500

    reading ahead

---
+++ src/CollectionsExamples.java
@@ -0,0 +1,40 @@
+public class CollectionsExamples {
+    public static void main(String[] args) {
+
+        // *** Collections *** \\
+
+        //Collection = A data structure that can be used to group,
+        // or collect objects. The 'Java standard library', a
+        // collection of code that comes with the Java language,
+        // contains many collections to help with common
+        // programming tasks.
+
+        // The collection classes come from the 'java.util' package.
+        // The two most common are 'Arraylist', and 'HashMaps'.
+
+
+        // *** Array Lists *** \\
+
+        // ArrayList = a class that represents an array that can change
+        // it's size. All elements in an 'ArrayList' must be objects,
+        // and they must have the same type. Arrays in Java have a fixed
+        // length; meaning that once they are created they cannot change
+        // their length. In order to "add" an item to an array, you must
+        // make a copy of the array with a length bigger than the original.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
