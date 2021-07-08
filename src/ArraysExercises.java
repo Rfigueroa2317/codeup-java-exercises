f943df3e9b08c75ec5495f84857c80708e6dd57a
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-07 22:10:38 -0500

    update

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,11 +1,19 @@
+import java.util.Arrays;
 public class ArraysExercises {
 
     public static void main(String[] args) {
-        System.out.println("Arrays Exercise!");
 
         int[] numbers = {1,2,3,4,5};
-        System.out.println(numbers);
-    }
+        System.out.println("The numbers are: " + numbers);
+        // running the code like that won't give you the expected
+        // outcome. Instead, it will give you a random input
+        // of numbers and characters.
 
+        System.out.println(Arrays.toString(numbers));
+        // 'Arrays.toString' is necessary
+
+
+
+    }
 
 }
