def00191b5c3bb394330becca53c47c2c16340b9
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-07 20:58:55 -0500

    update

--- src/ArrayExamples.java
+++ src/ArrayExamples.java
@@ -15,6 +15,8 @@ public class ArrayExamples {
 //        Bug[] mycode = new Bug[numberOfBugs];
 
 
+        // *** Working with Array Elements *** \\
+
 //        int[] numbers = new int[3];
 //        numbers[0] = 1;
 //        numbers[1] = 2;
@@ -25,14 +27,56 @@ public class ArrayExamples {
 //        System.out.println(numbers[3]);
 
 
-        String[] beatles = {"John", "Paul", "Ringo"};
+        // In here we're using the array initializer syntax
+//        String[] beatles = {"John", "Paul", "Ringo"};
+//
+//        System.out.println(beatles.length);
+//
+//        System.out.println(beatles[0]);
+//        System.out.println(beatles[1]);
+//        System.out.println(beatles[2]);
 
-        System.out.println(beatles.length);
+        //ArrayIndexOutOfBoundsException !
+//        beatles[3] = "George";
 
-        System.out.println(beatles[0]);
-        System.out.println(beatles[1]);
-        System.out.println(beatles[2]);
 
+        // *** Iterating *** \\
+        // Like we've seen in JavaScript, we can use the 'length'
+        // property of any array in combination with a looping construct
+        // to iterate over it.
+
+//        String[] languages = {"html", "css", "javascript", "java"};
+
+//        for (int i = 0; i < languages.length; i += 1){
+//            System.out.println(languages[i]);
+//        }
+
+        // in addition to the traditional 'for' loop, java provides
+        // an enhanced 'for' that is similar to a 'forEach' in
+        // JavaScript. It is useful when we want to do something
+        // for every item in an array.
+
+//        String[] languages = {"html", "css", "javascript", "java"};
+//        for (String language: languages){
+//            System.out.println(language);
+
+            //This is the output for both 'for' loops:
+            //html
+            //css
+            //javascript
+            //java
+
+        // Be specific when using enhanced 'for' loop. we have to
+        // specify what collection we are iterating over, a variable
+        // to hold each element, and the type of each element
+        // in the collection.
+
+        int[] numbers = {1,2,3,4,5};
+        for (int n : numbers){
+            System.out.println(n);
+
+            //output is all the numbers in in the int numbers array.
+        }
 
     }
 
