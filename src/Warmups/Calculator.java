68757b84fc91413d040cb3e74dda4303b2fe4de4
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-08 09:09:57 -0500

    morning warmup

---
+++ src/Warmups/Calculator.java
@@ -0,0 +1,13 @@
+package Warmups; //Morning Warmup 7/8/21
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void CalculatorApp(){
+
+    }
+
+}
