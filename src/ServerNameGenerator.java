0330ef0d99880d73a1dfe36a494d676aa8535b42
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-07 22:57:10 -0500

    arrays

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,7 +1,23 @@
 public class ServerNameGenerator {
 
-    String[] adjectives = {};
-    String[] nouns = {};
+    // Adjective = A word or phrase naming an attribute, added to
+    // or grammatically related to a noun to modify or describe it.
+
+    // Noun = A word (other than a pronoun) used to identify any of
+    // a class of people, places, or things (common noun), or to
+    // name a particular one of these (proper noun).
+
+
+
+    String[] adjectives = {"agreeable","anxious","busy"
+            ,"blue","comfortable", "curious","distinct"
+            ,"excited","hungry","jittery"};
+    String[] nouns = {"people","history","art","world","system"
+            ,"computer","food","knowledge","power","ability"};
+
+
+
+
 
     public static void main(String[] args) {
 
