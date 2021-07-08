93269e08986e959cc0137b732cf77bd24c65d99b
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-07 20:43:59 -0500

    update

--- src/ArrayExamples.java
+++ src/ArrayExamples.java
@@ -15,14 +15,24 @@ public class ArrayExamples {
 //        Bug[] mycode = new Bug[numberOfBugs];
 
 
-        int[] numbers = new int[3];
-        numbers[0] = 1;
-        numbers[1] = 2;
+//        int[] numbers = new int[3];
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+//
+//        System.out.println(numbers[0]);
+//        System.out.println(numbers[1]);
+//        System.out.println(numbers[2]);
+//        System.out.println(numbers[3]);
 
-        System.out.println(numbers[0]);
-        System.out.println(numbers[1]);
-        System.out.println(numbers[2]);
-        System.out.println(numbers[3]);
+
+        String[] beatles = {"John", "Paul", "Ringo"};
+
+        System.out.println(beatles.length);
+
+        System.out.println(beatles[0]);
+        System.out.println(beatles[1]);
+        System.out.println(beatles[2]);
+
 
     }
 
