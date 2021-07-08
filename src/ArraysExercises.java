7e16c9357fa7fc40658179c86d55cab79d567b99
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-07 22:33:53 -0500

    update

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,6 +3,8 @@ public class ArraysExercises {
 
     public static void main(String[] args) {
 
+        // .1 Array Basics
+
         int[] numbers = {1,2,3,4,5};
         System.out.println("The numbers are: " + numbers);
         // running the code like that won't give you the expected
@@ -10,10 +12,28 @@ public class ArraysExercises {
         // of numbers and characters.
 
         System.out.println(Arrays.toString(numbers));
-        // 'Arrays.toString' is necessary
+        // 'Arrays.toString' is necessary in order to be able
+        // to see the actual array.
 
 
+        // Create an Array that holds 3 person objects.
 
+        String[] Persons = {"Ricardo", "Roxanne", "Ana"};
+        System.out.println(Persons.length);
+//        for (int i = 0; i < Person.length; i += 1){
+//            System.out.println(Person[i]);
+//        }
+
+        // With Enhance method \\
+        for (String Person: Persons) {
+            System.out.println(Person);
+        }
     }
+
+
+
+//    static void addPerson(){
+//
+//    }
 
 }
