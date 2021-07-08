1cb0bc8f25e866c925082a377c11093756099ece
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-08 16:35:09 -0500

    update

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,23 +1,25 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArraysExercises {
 
     public static void main(String[] args) {
 
         // .1 Array Basics
 
-        int[] numbers = {1,2,3,4,5};
-        System.out.println("The numbers are: " + numbers);
+//        int[] numbers = {1,2,3,4,5};
+//        System.out.println("The numbers are: " + numbers);
         // running the code like that won't give you the expected
         // outcome. Instead, it will give you a random input
         // of numbers and characters.
 
-        System.out.println(Arrays.toString(numbers));
+//        System.out.println(Arrays.toString(numbers));
         // 'Arrays.toString' is necessary in order to be able
         // to see the actual array.
 
 
         // Create an Array that holds 3 person objects.
-
+//
 //        String[] Persons = {"Ricardo", "Roxanne", "Ana"};
 //        System.out.println(Persons.length);
 //        for (int i = 0; i < Person.length; i += 1){
@@ -29,11 +31,26 @@ public class ArraysExercises {
 //            System.out.println(Person);
 //        }
 
+        Person person1 = new Person("Ricardo");
+        Person person2 = new Person("Roxanne");
+        Person person3 = new Person("Ana");
+
+        String[] persons = {person1.getName(), person2.getName(), person3.getName()};
+
+        for (String person: persons) {
+            System.out.println(person);
+        }
+//        person1.setName("Raul");
+//        System.out.println(Arrays.toString(persons));
+        Person person4 = new Person("Raul");
+        String[] newUpdatedPeople = addPerson(persons, person4);
+        System.out.println(Arrays.toString(newUpdatedPeople));
     }
 
-    static void addPerson(){
-        String[] Persons = {"Ricardo", "Roxanne", "Ana"};
-
+    public static String[] addPerson(String[] people, Person name){
+        String[] newArr = Arrays.copyOf(people, people.length + 1);
+        newArr[people.length] = name.getName();
+        return newArr;
     }
 
 }
--- src/Person.java
+++ src/Person.java
@@ -3,29 +3,35 @@ import java.util.Scanner;
 // .1
 
  public class Person {
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    private String name;
-//
-//    public String getName(){
-//        return name;
-//        // TODO: return the person's name
-//    }
-//
-//    public void setName(String Ricardo){
-//        // TODO: change the name property to the passed value
-//        this.name = "Ricardo";
-//
-//    }
-//
-//    public void sayHello(){
-//        // TODO: print a message to the console using
-//        //  the person's name
-//        System.out.println("Hello!, my name is %s!" + name);
+     private String name;
+    public static void main(String[] args) {
 
-//    }
+
+    }
+
+    public Person(){}
+
+     public Person(String name) {
+         this.name = name;
+     }
+
+     public String getName(){
+        return this.name;
+        // TODO: return the person's name
+    }
+
+    public void setName(String name){
+        // TODO: change the name property to the passed value
+        this.name = name;
+
+    }
+
+    public void sayHello(){
+        // TODO: print a message to the console using
+        //  the person's name
+        System.out.println("Hello!, my name is %s!" + name);
+
+    }
 
 
 }
