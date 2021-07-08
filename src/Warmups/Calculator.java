159a6f7090a769f5dfa4908e253399dccd4b453e
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-08 09:20:08 -0500

    update

--- src/Warmups/Calculator.java
+++ src/Warmups/Calculator.java
@@ -1,13 +1,40 @@
 package Warmups; //Morning Warmup 7/8/21
 
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
-
+        CalculatorApp();
     }
 
     public static void CalculatorApp(){
 
+        int num1 = 0;
+        int num2 = 0;
+        char operator;
+        double answer = 0.0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter first number: ");
+        num1 = scanner.nextInt();
+        System.out.println("Please enter second number: ");
+        num2= scanner.nextInt();
+        System.out.println("What operation? ");
+        operator = scanner.next().charAt(0);
+
+        switch (operator) {
+            case '+': answer = num1 + num2;
+                    break;
+            case '-': answer = num1 - num2;
+                    break;
+            case '*': answer = num1 * num2;
+                    break;
+            case '/': answer = num1/num2;
+                    break;
+        }
+        System.out.println(num1+" "+ operator + " " +" "+num2+ " = "+ answer);
     }
 
 }
