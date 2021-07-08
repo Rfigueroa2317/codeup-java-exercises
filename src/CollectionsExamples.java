1d2c99a3155865c2da79d2da8c9f721d00b84330
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-08 10:23:02 -0500

    update

--- src/CollectionsExamples.java
+++ src/CollectionsExamples.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CollectionsExamples {
     public static void main(String[] args) {
 
@@ -29,14 +31,48 @@ public class CollectionsExamples {
         // have methods that handle common array operations, as opposed to
         // using the Arrays helper class and passing the instance.
 
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(20); // adding an element
+        numbers.add(0, 21); // specifying an index
+        numbers.add(22);
+
+        System.out.println(numbers); // [21,20,22]
+
+        numbers.size(); // 3
+        numbers.get(2); // 22
+
+        numbers.indexOf(20); // 1
+        numbers.indexOf("cat"); // -1
+
+        // Some things to note in this example:
+        // .We specify the type of each element in the collection with
+        //  angle brackets, (<>).
+
+        // .Because 'ArrrayList' must contain objects, we specify 'Integer'
+        //  instead of int.
+
+        // .The 'add' method is overloaded; we can specify either an element
+        //  to add, or an element to add, and the index of where to add it.
+
+        //  .'ArrayList' don't have a length property, instead we use the size
+        //  method to find out how many elements are in the collection.
+
+        //  .Unlike native Java arrays, we can use 'System.out.println' to
+        //  show the content of the array.Java collections all have 'toString'
+        //  methods on them that allow us to easily inspect their contents.
 
 
+        // There are some common methods for working with 'ArrayList':
 
+        //  .contains = checks if a collection contains a given element.
 
+        //  .lastIndexOf = finds the last index of the given element,
+        //      -1 if not found.
 
+        //  .isEmpty = checks if the list is empty.
 
-
-
+        //  .remove = remove the first occurrence of an item, or an
+        //      item at a given index.
 
 
 
