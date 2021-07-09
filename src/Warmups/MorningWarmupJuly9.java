70387ee389824932302b5b9ba26c5f330f6d4e52
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-09 10:52:25 -0500

    update

--- src/Warmups/MorningWarmupJuly9.java
+++ src/Warmups/MorningWarmupJuly9.java
@@ -2,8 +2,8 @@ package Warmups;
 
 import java.util.Arrays;
 
-public class MorningWarmupJuly9 {
-}
+//public class MorningWarmupJuly9 {
+//}
 
 // TODO: Create a method which will return a String[] containing the user's favorite vacation spots
 //  -> Each time the user inputs a new vacation spot, reset the array to be one element longer in length
@@ -12,16 +12,16 @@ public class MorningWarmupJuly9 {
 //  -> When the user decides to finish inputting spots, return the String[].
 //  -> sout out the returned value!
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//    }
 
-    }
-
-public static String[] VacationSpots(String spots){
-    String Spot1 = "Boston";
-    String Spot2 = "Cleveland";
-    String Spot3 = "San Juan";
-
-}
+//public static String[] VacationSpots(String spots){
+//    String Spot1 = "Boston";
+//    String Spot2 = "Cleveland";
+//    String Spot3 = "San Juan";
+//
+//}
 
 //    public static String[] addPerson(String[] people, Person newPerson){
 //        String[] newArr = Arrays.copyOf(people, people.length + 1);
