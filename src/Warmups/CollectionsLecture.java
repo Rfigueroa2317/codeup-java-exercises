35899dfc50e40fd21f0e685eaf7d0bfb54ec7a94
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-09 14:00:39 -0500

    update

--- src/Warmups/CollectionsLecture.java
+++ src/Warmups/CollectionsLecture.java
@@ -1,12 +1,14 @@
 package Warmups;
 
+import oop.Person;
+
 import java.util.ArrayList;
 
 public class CollectionsLecture {
 
     public static void main(String[] args) {
 
-        ArrayList<String> stringArr = new ArrayList<>();
+        ArrayList<String> stringArr = new ArrayList<>(), stringArrCopy = new ArrayList<>();
 
         stringArr.add("Ricardo");
 
@@ -21,8 +23,32 @@ public class CollectionsLecture {
         System.out.println(stringArr);
         System.out.println(stringArr.hashCode());
 
-        stringArr.remove(1);
-        System.out.println(stringArr.hashCode());
+        // Remove second element
+//        stringArr.remove(1);
+//        System.out.println(System.identityHashCode(stringArr));
+
+        ArrayList<String> stringArrayCopy = stringArr;
+
+        System.out.println("Array list copy: ");
+        System.out.println(stringArrayCopy);
+        System.out.println("Copied list hascode: ");
+        System.out.println(System.identityHashCode(stringArrayCopy));
+        System.out.println("\n");
+
+        stringArrayCopy.remove(1);
+        System.out.println(stringArr);
+
+        System.out.println("The array list are equal: " + stringArr.equals(stringArrayCopy));
+
+        ArrayList<Person> peopleList = new ArrayList<>();
+
+//        peopleList.add(new Person("Ricardo Figueroa"));
+//
+//        for(Person person: peopleList){
+//            PersonUtils.printGreeting("Hello, I am: ", person.getFullName());
+//        }
+
+
 
     }
 }
