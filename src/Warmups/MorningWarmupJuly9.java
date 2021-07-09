d99253b27601ac246bb49c6c5a3e779d312c0bd6
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-09 09:10:41 -0500

    update

---
+++ src/Warmups/MorningWarmupJuly9.java
@@ -0,0 +1,11 @@
+package Warmups;
+
+public class MorningWarmupJuly9 {
+}
+
+// TODO: Create a method which will return a String[] containing the user's favorite vacation spots
+//  -> Each time the user inputs a new vacation spot, reset the array to be one element longer in length
+//  -> Then, add the old elements
+//  -> Finally, add the new element
+//  -> When the user decides to finish inputting spots, return the String[].
+//  -> sout out the returned value!
