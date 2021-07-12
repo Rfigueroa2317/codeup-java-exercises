c34a9e50b49a4b684a157d1962a20f246c623b95
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-12 12:37:46 -0500

    update

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -13,17 +13,23 @@ public class GradesApplication {
 
         System.out.println(studentOne.getGradeAverage());
 
-        HashMap<String, String> students = new HashMap<>();
+        // created hashMap with the key = string, value = Student instance
+        HashMap<String, Student> students = new HashMap<>();
         // how to assign the github username as a key within the HashMap
         // how to assign a `Student` object as the value
         // how to save 3 grades inside of the `Student` obj
 
-        students.put("Ryan", "ryanorsinger");
+
+        // 1. create a new instance of a Student class (Student one = new Student())
+        // 2. one.addGrades(88) to the `one` student instance
+        // 3. students.put(username, one)
+        students.put("ryanorsinger", );
         students.put("Zach","zgulde");
         students.put("Fernando", "fmendozaro");
         students.put("Justin", "jreich5");
 
-        {"ryanorsinger" {grades: 90, 80, 70}}
+
+//        {"ryanorsinger" {grades: 90, 80, 70}}
 
         System.out.println(students);
     }
