d410789e49ac8a53540fffef308b2a6f71b18c2f
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-13 11:18:50 -0500

    update

--- src/Annotations/AnnotationsExample.java
+++ src/Annotations/AnnotationsExample.java
@@ -2,4 +2,15 @@ package Annotations;
 
 public class AnnotationsExample {
 
+    // Annotations are used to provide metadata for Java code. Annotations don't
+    // modify the execution of code; however, they can be consumed in order to
+    // perform additional logic.
+
+    // A Java annotation, in its shortest form, looks like this:
+
+    // @Entity
+
+    // *** The @ character signals to the compiler that this is an annotation. The
+    // name following the @ character is the name of the annotation. ***
+
 }
