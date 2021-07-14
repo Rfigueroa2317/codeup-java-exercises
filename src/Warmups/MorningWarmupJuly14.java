b4844a428b13ac627da1641e701756971964e883
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-14 09:42:44 -0500

    update

---
+++ src/Warmups/MorningWarmupJuly14.java
@@ -0,0 +1,27 @@
+package Warmups;
+
+public class MorningWarmupJuly14 {
+    public static void main(String[] args) {
+        // TODO: warmup: write a method that accepts a string and
+        //  returns a String array that splits at the spaces -
+        //  you can write this within any main method.
+
+
+//        String s = "Hello There!";
+//        String[] ss = s.split("_");
+//        String smod = ss[0];
+//        System.out.println(smod);
+
+        String string ="one, two, three, four";
+    }
+
+//    public static void sprintArr(String){
+//        String[] name = String.split(" ");
+//
+//        for(int i = 0; i < name.length; i++){
+//            System.out.println(name[i]);
+//        }
+//    }
+
+
+}
