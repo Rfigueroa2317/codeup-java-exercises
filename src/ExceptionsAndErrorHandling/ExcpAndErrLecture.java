a029f813a24795a4d7da3153dfcca4c61b8d24b8
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-14 11:22:12 -0500

    update

--- src/ExceptionsAndErrorHandling/ExcpAndErrLecture.java
+++ src/ExceptionsAndErrorHandling/ExcpAndErrLecture.java
@@ -11,13 +11,17 @@ public class ExcpAndErrLecture {
 //        int myNum = Integer.parseInt("eighteen");
 
         // ARRAY EXCEPTION
-        ArrayList<Integer> myArrList = new ArrayList<Integer>();
-        myArrList.get(2);
+//        ArrayList<Integer> myArrList = new ArrayList<Integer>();
+//        myArrList.get(2);
 
 
         // THROW
-        impossible(8,1);
+//        impossible(8, 1);
+
+        newBankAccount(201, 17);
     }
+
+
     // Exception = a way for Java to handle our any errors in our code
     // and instance of one of several exception classes
     // Exception is gonna be thrown to us in the flow of our code
@@ -32,13 +36,30 @@ public class ExcpAndErrLecture {
 
 
     // THROW syntax
-    public static void impossible(int num1, int num2){
-        if(num2 == 1){
-            // throw new
-            throw new ArithmeticException();
-        }else{
-            System.out.println(num1 / num2);
-        }
+//    public static void impossible(int num1, int num2) {
+//        if (num2 == 1) {
+//            // throw new
+//            throw new ArithmeticException();
+//        } else {
+//            System.out.println(num1 / num2);
+//        }
+//    }
 
+    // TODO: Let’s say that you’re writing a program that checks
+    //  whether or not a user is eligible to open up a new bank account.
+    //  The requirements are that they have to be older than 16 years
+    //  old and have at least $200 to start their bank account.
+    //  Write a method that will return a string that states whether
+    //  they’re eligible to bank at Banco di Laura or whether they are
+    //  not eligible, make sure to  throw an `NumberFormatException` if
+    //  the user inputs anything that’s not a number (none, zero, zilch, nada, etc).
+
+    public static void newBankAccount(int Acct, int Age){
+        if((Age <= 16) && (Acct <= 200)){
+            throw new ArithmeticException();
+        }else {
+            System.out.println("Congrats you qualify!");
+        }
     }
+
 }
