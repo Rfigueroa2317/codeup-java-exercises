d7531023e3020dd7a7455d1a1389b6e8ae9a3200
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-15 14:13:13 -0500

    update

---
+++ src/IO/FileIOExample.java
@@ -0,0 +1,18 @@
+package IO;
+
+public class FileIOExample {
+
+    // *** File IO *** \\
+
+    // Some context and definitions:
+
+    //  .file = a discrete container for data on a storage device
+    //  .path = the location of a file in a filesystem
+
+    // IO = In Java, it provides a set of input streams and a set of output
+    //      streams used to read and write data to files or other input
+    //      and output sources. There are three categories of classes in
+    //      java.io: input streams, output streams and everything else.
+
+
+}
