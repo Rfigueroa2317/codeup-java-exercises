a1f127a4023a03a7e36e9880e4a41c071f755667
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-15 09:18:23 -0500

    update

---
+++ src/Warmups/WarmupJuly15.java
@@ -0,0 +1,26 @@
+package Warmups;
+
+import java.util.Random;
+
+public class WarmupJuly15 {
+
+    //WARM UP
+// TODO Given this string: `String str = "Oberon is the
+//  best cohort ever!"`, print out the character from that
+//  string using a random number generator, that generates
+//  a number between 0 and 50. Using a try/catch block, handle
+//  any errors (Exceptions) that may occur by printing out the
+//  message.
+
+    public static void main(String[] args) {
+
+        String str = "Oberon is the best cohort ever!";
+        Random random = new Random();
+
+        for (int i = 0; i < str.length(); i++) {
+
+        }
+
+
+    }
+}
