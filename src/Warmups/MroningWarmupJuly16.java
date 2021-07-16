694a1bce196a2830df76e4373dcd8b054ef2be74
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-16 09:21:45 -0500

    moring push

---
+++ src/Warmups/MroningWarmupJuly16.java
@@ -0,0 +1,29 @@
+package Warmups;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class MroningWarmupJuly16 {
+
+    // Write a method named getTwentyPrimes()
+    // that returns a string containing the first 20
+    // prime numbers, each separated by a comma.
+    // Output: "1, 2, 3, 5, 7, 11, 13, 17, 19"...
+    // until we have a total count of 20 primes
+    // in the string.
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int prime = [1, 20];
+        PrimeNumber(prime);
+    }
+
+
+    public static int PrimeNumber(int prime){
+        for (int i = 0; i < 20; i++) {
+            if((prime == 2)){
+            }
+        }
+    }
+}
