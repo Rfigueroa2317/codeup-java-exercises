40ad0207275c772e731ed77da6bf8d2c417cad9a
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-17 20:06:22 -0500

    update

--- src/fileIO/FileIOExample.java
+++ src/fileIO/FileIOExample.java
@@ -15,4 +15,60 @@ public class FileIOExample {
     //      java.io: input streams, output streams and everything else.
 
 
+    // *** Absolute vs Relative Paths *** \\
+
+    // Paths consist of directory names, separated by '/s'. In addition there
+    // are several special characters in paths:
+
+    //  . '/': when a path starts with '/', it refers to the filesystem root,
+    //  otherwise it is used to separate directories in a path.
+    //  . '.': current directory
+    //  . '..': parent directory
+    // *** you know these already as you use them on intellij to navigate
+    // through the terminal for files *** \\
+
+    // There are two types of paths:
+
+    //  .Absolute paths = specified from the filesystem root
+    //  .relative paths = relative to the 'current working directory'
+
+    // imagine we have a filesystem that looks like this:
+
+    //  /
+    //      users/
+    //          Document/
+    //              important-stuff.tx
+    //          IdeaProjects/
+    //              java-exercises/ <-- current working directory
+    //                  src/
+    //                      FileTastic.java
+    //                  data/
+    //                      data.txt
+
+    // The directory that we are operating from, or the 'current working
+    // directory', is marked as the 'java-exercises' project directory.
+    // Since this is the case, here are some examples of relative paths:
+
+    //  data/data.txt
+    //  ./data/.txt
+    //  ../../Documents/important-stuff.txt
+
+    // We say that these paths are relative, because their location on
+    // where the current working directory is, where we are specifying
+    // the path from. If we specified these paths from a different
+    // working directory, they would be invalid, meaning they wouldn't
+    // be correct paths to the files they were referencing.
+
+    // In contrast, absolute paths specify a location 'unambiguously'.
+    // These paths:
+
+    //  /Users/codeup/IdeaProjects/java-exercises/data/data.txt
+    //  /Users/codeup/Documents/important-stuff.txt
+
+    // Will always refer to the same location, regardless of where the
+    // current working directory is.
+
+    // *** TO BE CONTINUED *** \\
+
+
 }
