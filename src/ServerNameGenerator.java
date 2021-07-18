8ef0e660d2be2fbb65b0a3d89b5f99689a558957
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-18 14:59:27 -0500

    update

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class ServerNameGenerator {
 
     // Adjective = A word or phrase naming an attribute, added to
@@ -7,17 +10,31 @@ public class ServerNameGenerator {
     // a class of people, places, or things (common noun), or to
     // name a particular one of these (proper noun).
 
-
-
-    String[] adjectives = {"agreeable","anxious","busy"
-            ,"blue","comfortable", "curious","distinct"
-            ,"excited","hungry","jittery"};
-    String[] nouns = {"people","history","art","world","system"
-            ,"computer","food","knowledge","power","ability"};
-
-
-
     public static void main(String[] args) {
+        GettingRandomElement();
     }
+
+    private static void GettingRandomElement() {
+        Random rand = new Random();
+        String[] adjectives = {"agreeable","anxious","busy"
+                ,"blue","comfortable", "curious","distinct"
+                ,"excited","hungry","jittery"};
+        String[] nouns = {"people","history","art","world","system"
+                ,"computer","food","knowledge","power","ability"};
+
+        for (String adjective : adjectives) {
+            System.out.println(adjective);
+        }
+
+        for (String noun : nouns) {
+            System.out.println(noun);
+        }
+
+        String RandomAdj = rand.toString();
+
+        System.out.println(Arrays.toString(adjectives));
+        System.out.println(Arrays.toString(nouns));
+    }
+
 
 }
