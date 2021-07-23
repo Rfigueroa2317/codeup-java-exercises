9f508005be92ef42c5e15305b51330a65b624def
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-22 22:09:37 -0500

    update

--- src/Shapes/ShapesTest.java
+++ src/Shapes/ShapesTest.java
@@ -7,6 +7,12 @@ public class ShapesTest {
         System.out.println(myShape.getArea());
         System.out.println(myShape.getPerimeter());
 
+        // 9. The interface in Measurable is what declares the
+        // getPerimeter method. If it's not implemented it wont work
+
+        // 10. When trying to call the getLength and getWidth methods you'll get
+        // a 'Cannot resolve symbol'. Both are defined within an Abstract class
+
         // *** NOT FINISHED *** \\
     }
 }
