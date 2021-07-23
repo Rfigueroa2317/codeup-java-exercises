7965da5a9a0b1f589d4341c6f59f851dc2a21650
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-23 10:31:28 -0500

    update

--- src/MySQLLecture.java
+++ src/MySQLLecture.java
@@ -31,4 +31,33 @@ public class MySQLLecture {
     // . Check the status with:
     // mysql.
 
+
+    // MySQL in Command Line
+    // . There are 2 parts to MySQL:
+    //      . MySQL server - understands SQL commands; this is where tha data
+    //                      lives
+    //      . MySQL client - the interface we use to connect to the server and
+    //                      send commands to display data on screen
+    // . We can connect to the database
+    // the command to connect to the terminal is:
+    // mysql -u root -p
+    // password: codeup
+    // you will not see it and that's intentional. It is for security purposes
+
+    // On Exercises:
+    // SELECT * FROM mysql.user will give you the list of users.
+    // ';' will not recognize the command. '\G' will give you the whole
+    // list of users.
+
+    // SELECT user, host FROM mysql.user
+    // ';' will give you the table. '\G' will give you the row.
+    //  Both the same information
+
+    // SELECT * FROM mysql.help_topic
+    // both commands will give you help topics. too long to see the difference.
+
+    // SELECT help_topic_id, help_category_id, url FROM mysql.help_topic
+    // Same as the ones above. One will give you the table option,
+    // The other one will give you the row option. Both giving you
+    // the same info.
 }
