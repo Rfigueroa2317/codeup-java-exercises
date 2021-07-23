db294fe2f0b9fadfb821616ce8de00e48c821abf
ricardofigueroa <rfigueroa2317@gmail.com>
2021-07-23 09:33:53 -0500

    update

---
+++ src/MySQLLecture.java
@@ -0,0 +1,34 @@
+public class MySQLLecture {
+
+    // What is Data?
+
+    // .Facts
+
+    // what is a Database?
+    // . A systematic collection of data
+    // . Databases allow us to easily manage and sort through
+    //  organized data
+    // . DBMS - A collection of programs which allow users to access
+    //      a database and manipulate data. Data-Based-Management-System
+    // some places who use DBMS:
+    //      . CPS Energy - uses DBMS to manage billing, client info
+    //      . FaceBook
+
+    // What is MySQL?
+
+    // . MySQL is a Relational Database Management System (RDBMS)
+    // . Allows data between tables to communicate with each other
+    // . Lets us manipulate millions of rows efficiently
+
+    // . Relation databases are the most common way to permanently store
+    //   data in web development
+    // . SQL is the language we use to query and interact with our database.
+    // . The commands in this language are divided into several categories.
+
+    // Checking the server
+    // . Before we interact with our database, we must make sure that the
+    // server is running.
+    // . Check the status with:
+    // mysql.
+
+}
