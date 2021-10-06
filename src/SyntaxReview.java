85a0bf5f878206bb33b36ca131db9d9dcdd3a149
ricardofigueroa <rfigueroa2317@gmail.com>
2021-10-05 23:11:46 -0500

    practicing and reviewing

---
+++ src/SyntaxReview.java
@@ -0,0 +1,20 @@
+public class SyntaxReview {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+
+        int myOtherFavoriteNumber = 20;
+        System.out.println(myOtherFavoriteNumber);
+
+        int myAllTimeFavoriteNumber = 200;
+        System.out.println(myAllTimeFavoriteNumber);
+
+//        String myString = "Cat";
+//        System.out.println(myString);
+
+        String anotherAnimalName = "Noodles";
+        System.out.println(anotherAnimalName);
+
+        // This are simply for warmup and getting back into the flow of java. It will get complicated \\
+    }
+}
