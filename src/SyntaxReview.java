17c66d5715ecf6bb8beec1dd035db4fe86773074
ricardofigueroa <rfigueroa2317@gmail.com>
2021-10-06 10:45:36 -0500

    warmups

--- src/SyntaxReview.java
+++ src/SyntaxReview.java
@@ -15,6 +15,20 @@ public class SyntaxReview {
         String anotherAnimalName = "Noodles";
         System.out.println(anotherAnimalName);
 
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(y *= x);
+
+        int x = 10;
+        int y = 2;
+        System.out.println(x /= y);
+        System.out.println(y -= x);
+
+
         // This are simply for warmup and getting back into the flow of java. It will get complicated \\
     }
 }
