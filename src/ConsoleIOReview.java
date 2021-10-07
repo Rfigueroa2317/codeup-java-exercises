b88310563308b70d3418d558886a158cd9a78bc9
ricardofigueroa <rfigueroa2317@gmail.com>
2021-10-06 22:25:25 -0500

    entry level practice

---
+++ src/ConsoleIOReview.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ConsoleIOReview {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter something: ");
+//        String userInput = scanner.next();
+//
+//        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+        String userInput = scanner.nextLine();
+
+        double pi = 3.14159;
+        System.out.println("The output of pi is approximately 3.14");
+        System.out.format(userInput);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/HelloWorldUdemyCourse.java
@@ -0,0 +1,13 @@
+public class HelloWorldUdemyCourse {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        // challenge #1
+        String name = "Ricardo";
+
+        System.out.println("Hello " + name);
+
+        //answer is simply change the word "World" to your first name
+    }
+}
