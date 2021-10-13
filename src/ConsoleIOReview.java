b15f90fb2f494d018a66e17414182a273f79b80f
ricardofigueroa <rfigueroa2317@gmail.com>
2021-10-13 08:06:57 -0500

    today's push

--- src/ConsoleIOReview.java
+++ src/ConsoleIOReview.java
@@ -22,8 +22,12 @@ public class ConsoleIOReview {
             return;
         }
 
-
-
+        for(int i =1; i <= 10; i++) {
+            if(i % 2 ==1){
+                continue;
+            }
+            System.out.println(i);
+        }
 
 
     }
